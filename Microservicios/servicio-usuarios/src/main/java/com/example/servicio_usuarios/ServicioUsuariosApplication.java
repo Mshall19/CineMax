42c375b8a7bc0019af4c9fad799d8c0647050368
Mshall19 <juandapeva282@gmail.com>
2025-12-07 18:55:07 -0500

    configuracion del docker y la base de datos

--- Microservicios/servicio-usuarios/src/main/java/com/example/servicio_usuarios/ServicioUsuariosApplication.java
+++ Microservicios/servicio-usuarios/src/main/java/com/example/servicio_usuarios/ServicioUsuariosApplication.java
@@ -3,11 +3,11 @@ package com.example.servicio_usuarios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// SIN EXCLUDES, SIN IMPORTS RAROS DE SECURITY
 @SpringBootApplication
 public class ServicioUsuariosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServicioUsuariosApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioUsuariosApplication.class, args);
+    }
 }
