291b396e3cb590a3704d55f97a10b4e1f3e47483
Mshall19 <juandapeva282@gmail.com>
2025-12-07 15:46:16 -0500

    estructura inicial

---
+++ Microservicios/servicio-proyecciones/src/test/java/com/example/servicio_proyecciones/ServicioProyeccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicio_proyecciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioProyeccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
