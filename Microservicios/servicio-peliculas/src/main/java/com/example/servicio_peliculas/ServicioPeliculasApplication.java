1d4f782ca19cccbc813963d38805115d494fa63d
Mshall19 <juandapeva282@gmail.com>
2025-12-08 18:25:50 -0500

    configuracion de microservicio de peliculas

---
+++ Microservicios/servicio-peliculas/src/main/java/com/example/servicio_peliculas/ServicioPeliculasApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicio_peliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioPeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioPeliculasApplication.class, args);
+	}
+
+}
